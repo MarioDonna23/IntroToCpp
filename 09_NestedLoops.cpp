64d3b61389f74536a678c2588c072487ea691429
MarioDonna <mariodonna23@gmail.com>
2020-04-01 22:46:36 -0600

    Nested Loops

---
+++ 09_NestedLoops.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int x = 0; x < 4; x++)
+    {
+        for(int y = 0; y < 4; y++)
+        {
+            cout << y;
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
